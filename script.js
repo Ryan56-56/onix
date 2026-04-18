0d181155f684a00df3fdc22af90dd20fa3d782fd
Ryan56-56 <rjv21404@outlook.com>
2026-04-17 21:36:32 -0500

    Update ONNX model file path to model.onnx

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let session = null;
 async function loadModel() {
   try {
     // Make sure DLnet_Data.onnx is in the same folder as index.html and index.js
-    session = await ort.InferenceSession.create("./DLnet2026_WineData.onnx");
+    session = await ort.InferenceSession.create("./model.onnx?v=1");
     console.log("Model loaded successfully.");
   } catch (err) {
     console.error("Failed to load ONNX model:", err);
